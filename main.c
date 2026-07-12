39974a4065b19bfb3f291f5c705cbd42dc0fc3ac
Marco Blackwell <mblackwe@amd.com>
2026-07-12 14:55:59 +0100

    add dummy input function and change producer consumre model
    now assumes packets are not framed and not place in segmented slots
    by hardware.

--- main.c
+++ main.c
@@ -8,24 +8,73 @@ uint8_t IN_BUF[1024 * 1024] = {0};
 size_t MAX_PACKET_SIZE = 1024;
 size_t MAX_PACKETS_IN_FLIGHT = 1024;
 
+// start BABECAFE, end C0FFEE
+// len = payload_len + 15 (overhead)
+size_t PACKET_OVERHEAD = 15;
+// packet format = 0xBA, 0xBE, 0xCA, 0xFE, [uint32_t len], [payload], [CRC32 (0's if unused)], 0xC0, 0xFF, 0xEE
+
 typedef struct packet {
     uint8_t *data;
-    size_t size;
+    size_t len;
 } packet;
+
+void insert_dummy_packet(uint8_t **p, size_t len) {
+    // bounds checking first
+    if (len < PACKET_OVERHEAD) { printf("Packet cannot be shorter than the overhead.\n"); return; }
+    if (!*p) { printf("Unable to write into the producer queue, p is null.\n"); return; }
+    bool needs_wraparound = false;
+    if (*p + len > IN_BUF + MAX_PACKET_SIZE * MAX_PACKETS_IN_FLIGHT) needs_wraparound = true;
+    uint8_t start_delim[4] = {0xBA, 0xBE, 0xCA, 0xFE};
+    uint8_t end_delim[3] = {0xC0, 0xFF, 0xEE};
+    uint8_t blank_crc[4] = {0x00, 0x00, 0x00, 0x00};
+    uint8_t len_uint8[4];
+    uint8_t *len_addr = (uint8_t *) &len;
+    for (size_t i = 0; i < 4; i++) {
+        len_uint8[i] = len_addr[i];
+    }
+
+    switch (needs_wraparound) {
+        case false:
+            memcpy(*p, start_delim, 4); *p += 4;
+            memcpy(*p, len_uint8, 4); *p += 4;
+            for (size_t i = 0; i < len - PACKET_OVERHEAD; i++) {
+                *(*p)++ = i;
+            }
+            memcpy(*p, blank_crc, 4); *p += 4;
+            memcpy(*p, end_delim, 3); *p += 3;
+            break;
+        case true:
+            uint8_t staging_buffer[MAX_PACKET_SIZE];
+            uint8_t *stage_p = staging_buffer;
+            size_t space_available = IN_BUF + (MAX_PACKET_SIZE * MAX_PACKETS_IN_FLIGHT) - *p;
+            memcpy(staging_buffer, start_delim, 4); stage_p += 4;
+            memcpy(stage_p, len_uint8, 4); stage_p += 4;
+            for (size_t i = 0; i < len - PACKET_OVERHEAD; i++) {
+                *stage_p++ = i;
+            }
+            memcpy(stage_p, blank_crc, 4); stage_p += 4;
+            memcpy(stage_p, end_delim, 3); stage_p += 3;
+
+            memcpy(*p, staging_buffer, space_available); *p = IN_BUF;
+            stage_p = staging_buffer + space_available;
+            memcpy(*p, stage_p, len - space_available); *p += len - space_available;
+            break;
+    }
+    return;    
+}
 
 int main() {
     printf("Size of IN_BUF: %zu MB\n", sizeof(IN_BUF) / 1024 / 1024);
     uint8_t *p = IN_BUF;
     uint8_t *c = IN_BUF;
 
-    // packet format = 0xAB, [uint32_t len], [payload], 0xBA, 0xBA
     uint8_t data_arr[10] = {0xAB, 10, 0, 0, 0, 5, 6, 7, 0xBA, 0xBA};
     packet p1 = {
         .data = data_arr,
-        .size = 10
+        .len = 10
     };
 
-    memcpy(p, p1.data, p1.size);
+    memcpy(p, p1.data, p1.len);
     p += MAX_PACKET_SIZE;
 
     if (c != p) {
